8d918993e2ba142aa301d853711252ccc606c3fe
Neo-Nguyen-95 <dzung.usth@gmail.com>
2025-02-25 23:34:30 +0700

    Update main_embedding.py

--- main_embedding.py
+++ main_embedding.py
@@ -16,18 +16,6 @@ knowledge_text = kb_content.split('\n')
 knowledge_text.extend(qa_content.replace('\n', '').split('---'))
 
 #%% EMBEDDING
-client = OpenAI(api_key=OPENAI_API_KEY)
-
-def get_embedding(text):
-    response = client.embeddings.create(
-        input = text,
-        model='text-embedding-ada-002'
-        )
-    
-    embedding = response.data[0].embedding
-    
-    return np.array(embedding)
-
 knowledge_embeddings = np.array([get_embedding(text) for text in knowledge_text])
 
 dimension = knowledge_embeddings.shape[1]
